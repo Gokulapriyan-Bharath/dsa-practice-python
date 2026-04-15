96bb7fbdc495312182097d0935f8f14e4ab84e58
Gokulapriyan-Bharath <gokulapriyanbharath@gmail.com>
2026-04-15 20:43:37 +0530

    completed the Two Summ II

---
+++ arrays/Two Sum II.py
@@ -0,0 +1,17 @@
+def twoSum(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+
+        if current_sum == target:
+            return [left + 1, right + 1]
+
+        elif current_sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+print(twoSum(numbers=[2,7,11,15],target=9))
